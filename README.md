850713c658d7e2ae520803b0a8ec66f113422667
Alexandr Ludov <efraam@gmail.com>
2025-02-01 21:05:56 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ITKAchievementBot
+ 
---
+++ src/main/java/com/efr/ITKAchievementBot/ItkAchievementBotApplication.java
@@ -0,0 +1,13 @@
+package com.efr.ITKAchievementBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItkAchievementBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItkAchievementBotApplication.class, args);
+	}
+
+}
