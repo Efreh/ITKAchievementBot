b8e514653f0dd48fd9b267ab4b21ef056b88e2b6
Alexandr Ludov <107674810+Efreh@users.noreply.github.com>
2025-02-18 16:34:43 +0600

    Delete target directory

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS achievement_definition (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    title VARCHAR(255),
-    description VARCHAR(255),
-    type VARCHAR(50),
-    required_value INT,
-    required_keyword VARCHAR(255)
-);
